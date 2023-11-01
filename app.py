6e9433fd29c35e10b5514f6c38468ec04782ae78
Apparcane <brodiyla4@gmail.com>
2023-11-01 19:32:57 +0200

    Создана шапка и подвал сайта

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, url_for
+import requests
+
+SID = "oe2jhn59pl8gnu6sk2fj6o6o76"
+
 
 app = Flask(__name__)
 
@@ -6,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
